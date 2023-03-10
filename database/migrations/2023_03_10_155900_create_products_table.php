83a0fdb22e9f9d3e85dba3fcd3273f369ed73620
Barabas Maximilian <barabasmaximilian@gmail.com>
2023-03-10 17:42:13 +0100

    builed_image_path_storage_and_product_add_function

--- database/migrations/2023_03_10_155900_create_products_table.php
+++ database/migrations/2023_03_10_155900_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->string('brand');
+            $table->string('description');
+            $table->string('price');
+            $table->string('image')->default('0');
+            $table->string('imgpath')->default('0');
+
             $table->timestamps();
         });
     }
